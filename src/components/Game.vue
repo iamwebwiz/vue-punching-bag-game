9e0d41138a0773979f9e45f91eb2c57bdbf37b41
Ezekiel <iamwebwiz@gmail.com>
2019-08-08 11:50:31 +0100

    Initialized components

---
+++ src/components/Game.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- Image of bag -->
+    <img src="../assets/bag.png" alt="bag" />
+
+    <!-- Bag health -->
+
+    <!-- Game controls -->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "PunchingBagGame"
+  };
+</script>
