102fecf231739291d9851dc85cad432a6ae858e0
Ezekiel <iamwebwiz@gmail.com>
2019-08-11 09:06:59 +0100

    ready set go

---
+++ dist/precache-manifest.719b9ad6aaa9fcbc80b39c9622ad4f06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b22b020c72ecc0e49f06",
+    "url": "/js/chunk-vendors.5d275592.js"
+  },
+  {
+    "revision": "feab94c8c87b7a670d75",
+    "url": "/js/app.3884109b.js"
+  },
+  {
+    "revision": "224a26fe6a6737c76dc42ec711f1848b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef753770e96dade093ea0c1873c9d8da",
+    "url": "/img/bag.ef753770.png"
+  },
+  {
+    "revision": "135414266eb08be3bdf5269053fd6c00",
+    "url": "/img/bag-burst.13541426.png"
+  },
+  {
+    "revision": "feab94c8c87b7a670d75",
+    "url": "/css/app.4d15a398.css"
+  }
+];
